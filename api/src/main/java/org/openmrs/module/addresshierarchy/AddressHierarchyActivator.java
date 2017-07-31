a474bbe87fb7f4b633377ca1124667fa50b6fda2
salta-kozbagarova <salda.kozbagarova@gmail.com>
2017-07-31 19:24:18 +0600

    Non hierarchical address upload was added

---
+++ api/src/main/java/org/openmrs/module/addresshierarchy/AddressHierarchyActivator.java
@@ -0,0 +1,49 @@
+/**
+ * The contents of this file are subject to the OpenMRS Public License
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://license.openmrs.org
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+ * License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * Copyright (C) OpenMRS, LLC.  All Rights Reserved.
+ */
+package org.openmrs.module.addresshierarchy;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.openmrs.api.context.Context;
+import org.openmrs.module.BaseModuleActivator;
+import org.openmrs.module.ModuleActivator;
+import org.openmrs.module.addresshierarchy.config.AddressConfigurationLoader;
+import org.openmrs.module.addresshierarchy.service.AddressHierarchyService;
+
+/**
+ * This class contains the logic that is run every time this module
+ * is either started or shutdown
+ */
+public class AddressHierarchyActivator extends BaseModuleActivator implements ModuleActivator {
+
+	private Log log = LogFactory.getLog(this.getClass());
+
+	@Override
+	public void started() {
+		log.info("AddressHierarchy Module Started");
+		AddressConfigurationLoader.loadAddressConfiguration();
+	}
+
+	@Override
+	public void stopped() {
+		log.info("AddressHierarchy Module Stopped");
+	}
+
+	@Override
+    public void contextRefreshed() {
+        // initialize the full address cache on module startup
+        Context.getService(AddressHierarchyService.class).initializeFullAddressCache();
+    }
+	
+}
---
+++ omod/src/main/java/org/openmrs/module/addresshierarchy/web/controller/portlet/AddressLayoutPortletController.java
@@ -0,0 +1,81 @@
+package org.openmrs.module.addresshierarchy.web.controller.portlet;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.openmrs.api.context.Context;
+import org.openmrs.module.addresshierarchy.AddressHierarchyEntry;
+import org.openmrs.module.addresshierarchy.AddressHierarchyLevel;
+import org.openmrs.module.addresshierarchy.service.AddressHierarchyService;
+import org.openmrs.module.addresshierarchy.util.AddressHierarchyUtil;
+import org.springframework.web.servlet.ModelAndView;
+
+public class AddressLayoutPortletController extends org.openmrs.web.controller.layout.AddressLayoutPortletController {
+	
+	protected static final Log log = LogFactory.getLog(AddressLayoutPortletController.class);
+	
+	/** Overrides the handle request to use the custom address hierarchy jsp if in edit mode */
+	
+    @Override
+	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException,
+	                                                                                           IOException {
+	
+		ModelAndView mav = super.handleRequest(request, response);
+		
+		String originalPortletPath = mav.getViewName();
+		
+		AddressHierarchyService ahService = Context.getService(AddressHierarchyService.class);
+		
+		// we only want to override with our custom page if 1) override is enabled, 2) size="full", 
+		// 3) we aren't in view mode, and 4) we have defined address hierarchy levels
+		try {
+			@SuppressWarnings("unchecked")
+            Map<Object,Object> map = (Map<Object,Object>) mav.getModelMap().get("model");	
+			if (AddressHierarchyUtil.getGlobalPropertyAsBoolean("addresshierarchy.enableOverrideOfAddressPortlet") == true &&
+					map.containsKey("size") && ((String) map.get("size")).equals("full") &&
+					(!map.containsKey("layoutMode") || ((String) map.get("layoutMode")).equals("edit")) &&
+					ahService.getAddressHierarchyLevelsCount() > 0) {
+				
+				// get the ordered address hierarchy levels and add them to the map
+				List<AddressHierarchyLevel> levels = ahService.getOrderedAddressHierarchyLevels(false);
+				mav.getModelMap().addAttribute("hierarchyLevels", levels);
+
+				// Do not enforce required field validation when editing an address on the location admin page
+				boolean validateRequiredFields = !(request.getServletPath().equals("/WEB-INF/view/admin/locations/locationForm.jsp"));
+				mav.getModelMap().addAttribute("validateRequiredFields", validateRequiredFields);
+
+				// figure out at what point we need to switch to free text entry by iterating backwards
+				// through the levels until we find a level with entries
+				Integer i;
+				for (i = levels.size()-1; i > 0; i--) {
+					List<AddressHierarchyEntry> entries = Context.getService(AddressHierarchyService.class).getAddressHierarchyEntriesByLevel(levels.get(i));
+					if (entries != null && entries.size() > 0) {
+						break;
+					}
+				}
+				mav.getModelMap().addAttribute("switchToFreetext", i+1);
+				
+				// add the global property that specifies whether we should allow freetext entries for levels which have entries
+				mav.getModelMap().addAttribute("allowFreetext", AddressHierarchyUtil.getGlobalPropertyAsBoolean("addresshierarchy.allowFreetext"));
+				
+				// set the path to the custom page
+				String portletPath = "/module/addresshierarchy/portlets/addressLayout";
+				mav.setViewName(portletPath);
+			}
+		}
+		catch (Exception e) {
+			// if we run into an error, we only want to soft-fail and note that we were unable to override the portlet
+			log.error("Unable to override address portlet ", e);
+			mav.setViewName(originalPortletPath);
+		}
+		
+		return mav;
+	}
+}
